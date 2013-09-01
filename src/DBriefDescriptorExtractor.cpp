6047fdaae2d1a1a2219e052faaaa9b649db82e94
Andres Perez <gantzer89@gmail.com>
2013-09-01 14:58:47 +0200

    src/DBriefDescriptorExtractor.cpp * Fixed conversion of descriptor representation due to misunderstanding of binary descriptors in OpenCV where they are stored in chunks of 8 bits.

--- src/DBriefDescriptorExtractor.cpp
+++ src/DBriefDescriptorExtractor.cpp
@@ -28,17 +28,21 @@ void DBrief(const Mat& image, vector<KeyPoint>& keypoints, Mat& descriptors,
 	dbrief.getDbriefDescriptors(descs, keypoints, &ipl_grayImage);
 
 	descriptors.create((int) keypoints.size(), size, type);
+	// Initializing matrix with zeros
+	std::fill_n(descriptors.data, descriptors.rows * descriptors.cols, 0);
 
 	for (int i = 0; i < (int) descs.size(); i++) {
 		bitset<CVLAB::DESC_LEN> desc = descs[i];
-		// printf("String formatted: %s\n", desc.to_string<char, char_traits<char>, allocator<char> >().c_str());
+		//printf("String formatted: %s\n", desc.to_string<char, char_traits<char>, allocator<char> >().c_str());
 
 		// Doing a reverse loop because the most significant on a bitset is the last element
-		// e.g. 1101 is stored as 1011
+		// e.g. 1100 is stored as 0011
 		for (int j = (int) desc.size() - 1; j >= 0; j--) {
-			// Bit access with range check
-			descriptors.at<unsigned char>(i, ((int) desc.size() - 1) - j) =
-					(bool) desc.test(j);
+			// Incrementally composing descriptor by
+			// splitting the 32 length bitset in chunks of 8 bits (1 byte)
+			// and converting it into its uint representation
+			descriptors.at<unsigned char>(i, descriptors.cols - 1 - (int) j / 8) +=
+					(desc.test(j) << (j % 8));
 		}
 	}
 
@@ -54,7 +58,7 @@ DBriefDescriptorExtractor::~DBriefDescriptorExtractor() {
 
 int DBriefDescriptorExtractor::descriptorSize() const {
 	// D-Brief descriptor has length 32 bits or 4 Bytes
-	return CVLAB::DESC_LEN;
+	return CVLAB::DESC_LEN / 8;
 }
 
 int DBriefDescriptorExtractor::descriptorType() const {
