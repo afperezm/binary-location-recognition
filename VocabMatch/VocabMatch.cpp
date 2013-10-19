faff140098790691bd63fae91b9d83a000307e28
Andres Perez <gantzer89@gmail.com>
2013-10-19 11:34:52 +0200

    .gitignore * Ignored files resulting from VocabMatch execution.
    VocabMatch/VocabMatch.cpp
    * Copied functions for writing result to a file from VocaMatch in VocabTree2 library.
    * Implemented corresponding calls to the results printing functions.

--- VocabMatch/VocabMatch.cpp
+++ VocabMatch/VocabMatch.cpp
@@ -22,24 +22,38 @@
 
 double mytime;
 
+int BasifyFilename(const char *filename, char *base);
+
+void PrintHTMLHeader(FILE *f, int num_nns);
+
+void PrintHTMLRow(FILE *f, const std::string &query, cv::Mat& scores,
+		cv::Mat& perm, int num_nns, const std::vector<std::string> &db_images);
+
+void PrintHTMLFooter(FILE *f);
+
 int main(int argc, char **argv) {
 
-	if (argc < 6 || argc > 7) {
+	if (argc < 6 || argc > 8) {
 		printf("\nUsage:\n"
 				"\t%s <in.tree> <in.db.list> <in.query.list>"
-				" <num_nbrs> <matches.out> [results.html]\n\n", argv[0]);
+				" <num_nbrs> <matches.out> [results.html] [candidates.txt]\n\n",
+				argv[0]);
 		return EXIT_FAILURE;
 	}
 
 	char *tree_in = argv[1];
 	char *db_list_in = argv[2];
 	char *query_list_in = argv[3];
-	int num_nbrs = atoi(argv[4]);
+	uint num_nbrs = atoi(argv[4]);
 	char *matches_out = argv[5];
 	const char *output_html = "results.html";
+	const char *candidates_out = "candidates.txt";
 
 	if (argc >= 7)
 		output_html = argv[6];
+
+	if (argc >= 8)
+		candidates_out = argv[7];
 
 	boost::regex expression("^(.+)(\\.)(yaml|xml)(\\.)(gz)$");
 
@@ -158,14 +172,45 @@ int main(int argc, char **argv) {
 	cv::Mat imgDescriptors;
 	cv::Mat scores;
 
+	std::vector<int>::iterator it = std::max_element(db_landmarks.begin(),
+			db_landmarks.end());
+
+	int max_ld = *it;
+
+	FILE *f_match = fopen(matches_out, "w");
+	if (f_match == NULL) {
+		fprintf(stderr, "Error opening file [%s] for writing\n",
+				candidates_out);
+		return EXIT_FAILURE;
+	}
+
+//	std::ofstream candidates(candidates_out, std::fstream::out);
+//	if (candidates.is_open() == false) {
+	FILE *f_candidates = fopen(candidates_out, "w");
+	if (f_candidates == NULL) {
+		fprintf(stderr, "Error opening file [%s] for writing\n",
+				candidates_out);
+		return EXIT_FAILURE;
+	}
+
+	FILE *f_html = fopen(output_html, "w");
+	PrintHTMLHeader(f_html, num_nbrs);
+	if (f_html == NULL) {
+		fprintf(stderr, "Error opening file [%s] for writing\n",
+				candidates_out);
+		return EXIT_FAILURE;
+	}
+
 	for (size_t i = 0; i < query_filenames.size(); i++) {
 		// Initialize keypoints and descriptors
 		imgKeypoints.clear();
 		imgDescriptors = cv::Mat();
+
 		// Load query keypoints and descriptors
 		FileUtils::loadFeatures(query_filenames[i], imgKeypoints,
 				imgDescriptors);
 
+		// Score query bow vector against db images bow vectors
 		mytime = cv::getTickCount();
 		try {
 			tree.scoreQuery(imgDescriptors, scores, db_filenames.size(),
@@ -174,17 +219,125 @@ int main(int argc, char **argv) {
 			fprintf(stderr, "%s\n", error.what());
 			return EXIT_FAILURE;
 		}
-
 		mytime = ((double) cv::getTickCount() - mytime) / cv::getTickFrequency()
 				* 1000;
 
+		// Print to standard output the matching scores between
+		// the query bow vector and the db images bow vectors
 		for (size_t j = 0; (int) j < scores.cols; j++) {
 			printf(
 					"   Match score between [%lu] query image and [%lu] DB image: %f\n",
 					i, j, scores.at<float>(0, j));
 		}
+
+		// Obtain indices of ordered scores
+		cv::Mat perm;
+		cv::sortIdx(scores, perm, cv::SORT_EVERY_ROW + cv::SORT_ASCENDING);
+
+		int top = MIN (num_nbrs, db_filenames.size());
+
+		// Sum one to the landmark index because its zero-based
+		std::vector<int> votes(max_ld + 1, 0);
+
+		// Accumulating landmark votes for the top scored images
+		for (size_t i = 0; (int) i < top; i++) {
+			votes[db_landmarks[perm.at<int>(0, i)]]++;
+		}
+
+		// Finding max voted landmark and the number of votes it got
+		int max_votes = 0;
+		int max_landmark = -1;
+		for (int j = 0; j < max_ld + 1; j++) {
+			if (votes[j] > max_votes) {
+				max_votes = votes[j];
+				max_landmark = j;
+			}
+		}
+
+		// Print to a file the ranked list of candidates ordered by score
+		fprintf(f_candidates, "%s", query_filenames[i].c_str());
+		for (int j = 0; j < top; j++) {
+			std::string d_base = db_filenames[perm.at<int>(0, j)];
+			fprintf(f_candidates, " %s", d_base.c_str());
+		}
+		fprintf(f_candidates, "\n");
+		fflush(f_candidates);
+
+		// Print to a file the matching information
+		fprintf(f_match, "%lu %d %d\n", i, max_landmark, max_votes);
+		fflush(f_match);
+		fflush(stdout);
+
+		// Print to a file the ranked list of candidates ordered by score in HTML format
+		PrintHTMLRow(f_html, query_filenames[i], scores, perm, top,
+				db_filenames);
 	}
+
+//	candidates.close();
+	fclose(f_candidates);
+	fclose(f_match);
+	PrintHTMLFooter(f_html);
+	fclose(f_html);
 
 	return EXIT_SUCCESS;
 }
 
+void PrintHTMLHeader(FILE *f, int num_nns) {
+	fprintf(f, "<html>\n"
+			"<header>\n"
+			"<title>Vocabulary tree results</title>\n"
+			"</header>\n"
+			"<body>\n"
+			"<h1>Vocabulary tree results</h1>\n"
+			"<hr>\n\n");
+
+	fprintf(f, "<table border=2 align=center>\n<tr>\n<th>Query image</th>\n");
+	for (int i = 0; i < num_nns; i++) {
+		fprintf(f, "<th>Match %d</th>\n", i + 1);
+	}
+	fprintf(f, "</tr>\n");
+}
+
+void PrintHTMLRow(FILE *f, const std::string &query, cv::Mat& scores,
+		cv::Mat& perm, int num_nns, const std::vector<std::string> &db_images) {
+	char q_base[512], q_thumb[512];
+	BasifyFilename(query.c_str(), q_base);
+	sprintf(q_thumb, "%s.thumb.jpg", q_base);
+
+	fprintf(f,
+			"<tr align=center>\n<td><img src=\"%s\" style=\"max-height:200px\"><br><p>%s</p></td>\n",
+			q_thumb, q_thumb);
+
+	for (int i = 0; i < num_nns; i++) {
+		char d_base[512], d_thumb[512];
+		BasifyFilename(db_images[perm.at<int>(0, i)].c_str(), d_base);
+		sprintf(d_thumb, "%s.thumb.jpg", d_base);
+
+		fprintf(f,
+				"<td><img src=\"%s\" style=\"max-height:200px\"><br><p>%s</p></td>\n",
+				d_thumb, d_thumb);
+	}
+
+	fprintf(f, "</tr>\n<tr align=right>\n");
+
+	fprintf(f, "<td></td>\n");
+	for (int i = 0; i < num_nns; i++)
+		fprintf(f, "<td>%0.5f</td>\n", scores.at<float>(0, i));
+
+	fprintf(f, "</tr>\n");
+}
+
+void PrintHTMLFooter(FILE *f) {
+	fprintf(f, "</tr>\n"
+			"</table>\n"
+			"<hr>\n"
+			"</body>\n"
+			"</html>\n");
+}
+
+int BasifyFilename(const char *filename, char *base) {
+	strcpy(base, filename);
+	base[strlen(base) - 4] = 0;
+
+	return 0;
+}
