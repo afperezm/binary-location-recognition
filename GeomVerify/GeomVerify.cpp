9e026bbc1365b0a023b9cef213f22847b4442233
Andres Perez <gantzer89@gmail.com>
2014-01-18 18:00:35 +0100

    GeomVerify/GeomVerify.cpp * Implemented stable sort keeping indices in order to sort candidates inliers. * Changed structures related to candidates inliers sort process. * Removed parameters useful for using direct index to approach nearest neighbors search. * Loading list of queries together with region coordiantes onto a Query structure. * Removed unused variables.

--- GeomVerify/GeomVerify.cpp
+++ GeomVerify/GeomVerify.cpp
@@ -32,48 +32,39 @@ double mytime;
 //		 - Obtain number of inliers
 //	 - Re-order list of candidates by its number of inliers
 
+template<typename T>
+void sortAndKeepIdx(std::vector<T>& values, std::vector<size_t>& indices,
+		int flags);
+
 int main(int argc, char **argv) {
 
-	if (argc < 11 || argc > 16) {
+	if (argc < 9 || argc > 13) {
 		printf(
 				"\nUsage:\n"
-						"\tGeomVerify <in.tree> <in.direct.index> "
+						"\tGeomVerify "
 						"<in.ranked.files.folder> <in.ranked.files.prefix> "
 						"<in.db.descriptors.list> <in.db.keypoints.folder> <in.queries.descriptors.list> <in.queries.keypoints.folder> "
 						"<out.re-ranked.files.folder> <in.top.candidates> "
-						"[in.type.binary:1] [in.topKeypoints:500] [in.ratio.thr:0.8] [im.min.matches:8] [in.ransac.thr:10]"
+						"[in.topKeypoints:500] [in.ratio.thr:0.8] [im.min.matches:8] [in.ransac.thr:10]"
 						"\n\n");
 		return EXIT_FAILURE;
 	}
 
-	std::string in_tree = argv[1];
-	std::string in_direct_index = argv[2];
-	std::string in_ranked_lists_folder = argv[3];
-	std::string in_prefix = argv[4];
-	std::string in_db_desc_list = argv[5]; // Used to obtain the mapping dbimg.candidate -> dbimg.id
-	std::string in_db_keys_folder = argv[6]; // Used to load query ranked candidates
-	std::string in_queries_desc_list = argv[7]; // Quantized in tree to match keypoints represented by the same word
-	std::string in_queries_keys_folder = argv[8];
-	std::string out_ranked_lists_folder = argv[9];
+	std::string in_ranked_lists_folder = argv[1];
+	std::string in_prefix = argv[2];
+	std::string in_db_desc_list = argv[3]; // Used to obtain the mapping dbimg.candidate -> dbimg.id
+	std::string in_db_keys_folder = argv[4]; // Used to load query ranked candidates
+	std::string in_queries_desc_list = argv[5]; // Quantized in tree to match key-points represented by the same word
+	std::string in_queries_keys_folder = argv[6];
+	std::string out_ranked_lists_folder = argv[7];
 
-	int topCandidates = atoi(argv[10]);
-	bool isBinary = argc >= 12 ? atoi(argv[11]) : true;
-	int topKeypoints = argc >= 13 ? atoi(argv[12]) : 500;
-	double ratioThreshold = argc >= 14 ? atof(argv[13]) : 0.8;
-	int ransacMinMatches = argc >= 15 ? atoi(argv[14]) : 8;
-	double ransacThreshold = argc >= 16 ? atof(argv[15]) : 10.0;
+	int topCandidates = atoi(argv[8]);
+	int topKeypoints = argc >= 10 ? atoi(argv[9]) : 500;
+	double ratioThreshold = argc >= 11 ? atof(argv[10]) : 0.8;
+	int ransacMinMatches = argc >= 12 ? atoi(argv[11]) : 8;
+	double ransacThreshold = argc >= 13 ? atof(argv[12]) : 10.0;
 
 	// Step 1/4: load tree + direct index
-	cv::Ptr<bfeat::VocabTreeBase> tree;
-	cv::Ptr<bfeat::DirectIndex> directIndexQueries = new bfeat::DirectIndex();
-	cv::Ptr<bfeat::DirectIndex> directIndexCandidates =
-			new bfeat::DirectIndex();
-
-	if (isBinary == true) {
-		tree = new bfeat::VocabTreeBin();
-	} else {
-		tree = new bfeat::VocabTreeReal();
-	}
 
 	printf(
 			"-- Running spatial verification using topCandidates=[%d] topKeypoints=[%d] "
@@ -81,56 +72,13 @@ int main(int argc, char **argv) {
 			topCandidates, topKeypoints, ratioThreshold, ransacMinMatches,
 			ransacThreshold);
 
-	printf("-- Loading tree from [%s]\n", in_tree.c_str());
-	mytime = cv::getTickCount();
-	tree->load(in_tree);
-	mytime = ((double) cv::getTickCount() - mytime) / cv::getTickFrequency()
-			* 1000;
-	printf("   Loaded in [%lf] ms, got [%lu] words \n", mytime, tree->size());
-
-	printf("-- Loading direct index [%s]\n", in_direct_index.c_str());
-	mytime = cv::getTickCount();
-	directIndexCandidates->load(in_direct_index);
-	mytime = ((double) cv::getTickCount() - mytime) / cv::getTickFrequency()
-			* 1000;
-	printf("   Loaded in [%lf] ms, got [%lu] images\n", mytime,
-			directIndexCandidates->size());
-
 	// Step 2a: load list of queries descriptors
 	printf("-- Loading list of queries descriptors\n");
-	std::vector<std::string> queries_desc_list;
-	FileUtils::loadList(in_queries_desc_list, queries_desc_list);
+	std::vector<FileUtils::Query> queries_desc_list;
+	FileUtils::loadQueriesList(in_queries_desc_list, queries_desc_list);
 	printf("   Loaded, got [%lu] entries\n", queries_desc_list.size());
 
 	// Step 2b: load queries descriptors
-
-	uint wordIdx; // unused
-	double wordWeight; // unused
-	int nodeAtL, imgIdx = 0;
-	cv::Mat queryDescriptors;
-
-	tree->setDirectIndexLevel(
-			tree->getDepth() - 1 - directIndexCandidates->getLevel());
-
-	printf(
-			"-- Loading queries descriptors and adding them to the direct index, level=[%d]\n",
-			tree->getDirectIndexLevel());
-	// Loop over list of queries descriptors
-	for (std::string descriptorsFilename : queries_desc_list) {
-		// Load query descriptors
-		FileUtils::loadDescriptors(descriptorsFilename, queryDescriptors);
-		// Loop over query descriptors
-		for (int i = 0; i < queryDescriptors.rows; ++i) {
-			// Propagate descriptor down tree
-			tree->quantize(queryDescriptors.row(i), wordIdx, wordWeight,
-					nodeAtL);
-			// Add it to a new image to the direct index
-			directIndexQueries->addFeature(imgIdx, nodeAtL, i);
-		}
-		++imgIdx;
-	}
-	printf("   Loaded and added descriptors of [%lu] images\n",
-			directIndexCandidates->size());
 
 	// Step 3a: load list of queries key-points
 
@@ -151,35 +99,35 @@ int main(int argc, char **argv) {
 
 	std::vector<cv::DMatch> matchesCandidateToQuery, inlierMatches;
 	std::vector<cv::Point2f> matchedCandidatePoints, matchedQueryPoints;
-	int candidateImgId, queryImgId, top = -1;
+	int top = -1;
 
-	cv::Mat inliers_idx, candidates_inliers, candidates_inliers_idx, H;
+	cv::Mat inliers_idx, H;
+	std::vector<int> candidates_inliers;
+	std::vector<size_t> candidates_inliers_idx;
 
 	cv::Mat imgOut;
 	cv::Mat queryImg, candidateImg;
 	cv::Mat candidateDescriptors;
+	cv::Mat queryDescriptors;
 
 	std::string queryBase, candidateBase;
 
 	cvflann::Logger::setDestination("inliers.log");
 
-	HtmlResultsWriter::getInstance().open("results_gv.html", topCandidates);
-
 	// Loop over list of queries key-points
 	for (size_t i = 0; i < queries_desc_list.size(); ++i) {
 		printf("-- Processing query [%lu] - [%s]\n", i,
-				queries_desc_list[i].c_str());
-		queryImgId = i;
+				queries_desc_list[i].name.c_str());
 
-		queryBase = queries_desc_list[i].substr(8,
-				queries_desc_list[i].length() - 16);
+		queryBase = queries_desc_list[i].name.substr(8,
+				queries_desc_list[i].name.length() - 16);
 
 		// Step 4a: load and pre-process query features
 		printf("   Load and pre-process query features\n");
 		FileUtils::loadKeypoints(
 				in_queries_keys_folder + "/" + queryBase + "_kpt.yaml.gz",
 				queryKeypoints);
-		FileUtils::loadDescriptors(queries_desc_list[i], queryDescriptors);
+		FileUtils::loadDescriptors(queries_desc_list[i].name, queryDescriptors);
 		filterFeatures(queryKeypoints, queryDescriptors, topKeypoints);
 
 		// Step 4b: load list of query ranked candidates
@@ -197,7 +145,8 @@ int main(int argc, char **argv) {
 
 		top = MIN(int(ranked_candidates_list.size()), topCandidates);
 
-		candidates_inliers = cv::Mat::zeros(1, top, cv::DataType<int>::type);
+		candidates_inliers.clear();
+		candidates_inliers.resize(top, 0);
 
 		queryImg = cv::imread("oxbuild_images/" + queryBase + ".jpg",
 				CV_LOAD_IMAGE_GRAYSCALE);
@@ -212,7 +161,7 @@ int main(int argc, char **argv) {
 //		cv::waitKey(0);
 
 		// Step 4c: load and pre-process candidate features
-		for (size_t j = 0; int(j) < top; ++j) {
+		for (int j = 0; j < top; ++j) {
 
 			printf("   Load and pre-process candidate features\n");
 			FileUtils::loadKeypoints(
@@ -226,7 +175,7 @@ int main(int argc, char **argv) {
 
 			// Searching putative matches
 			printf("   Matching key-points of query [%lu] "
-					"against candidate [%lu]\n", i, j);
+					"against candidate [%d]\n", i, j);
 
 			// Id of database image
 			std::vector<std::string>::iterator it = std::find(
@@ -237,8 +186,6 @@ int main(int argc, char **argv) {
 				throw std::runtime_error("Candidate [%s] not found "
 						"in list of database filenames");
 			}
-
-			candidateImgId = std::distance(db_desc_list.begin(), it);
 
 			candidateBase = ranked_candidates_list[j];
 			candidateImg = cv::imread(
@@ -304,13 +251,14 @@ int main(int argc, char **argv) {
 //			cv::waitKey(0);
 
 			if ((int(matchesCandidateToQuery.size())) < ransacMinMatches) {
-				fprintf(stderr, "   Cannot compute homography, "
-						"at least [%d] putative matches are needed\n",
+				fprintf(stderr, "   Cannot compute homography between"
+						" query [%lu] and candidate [%d], "
+						"need at least [%d] putative matches\n", i, j,
 						ransacMinMatches);
 			} else {
 				// Compute a projective transformation between query and ranked file using direct index
 				printf("   Computing projective transformation "
-						"between query [%lu] and candidate [%lu]\n", i, j);
+						"between query [%lu] and candidate [%d]\n", i, j);
 
 				inliers_idx = cv::Mat();
 
@@ -328,7 +276,7 @@ int main(int argc, char **argv) {
 						"   Computed homography in [%0.3fs], found [%d] inliers\n",
 						mytime, numInliers);
 
-				candidates_inliers.at<int>(0, j) = numInliers;
+				candidates_inliers[j] = numInliers;
 
 				inlierMatches.clear();
 				for (int i = 0; i < inliers_idx.rows; ++i) {
@@ -342,6 +290,7 @@ int main(int argc, char **argv) {
 				cv::imwrite(
 						out_ranked_lists_folder + "/match_" + queryBase + "_"
 								+ candidateBase + ".jpg", imgOut);
+
 //				cv::namedWindow(
 //						"oxbuild_images/match_" + queryBase + "_"
 //								+ candidateBase + ".jpg", cv::WINDOW_NORMAL);
@@ -358,20 +307,21 @@ int main(int argc, char **argv) {
 			cvflann::Logger::log(0,
 					"query=[%s] candidate=[%s] numberInliers=[%d]\n",
 					queryBase.c_str(), candidateBase.c_str(),
-					candidates_inliers.at<int>(0, j));
+					candidates_inliers[j]);
 
 		}
 
 		printf("-- Re-ranking candidates list\n");
 
 		// Re-order list of candidates by its inlier number
-		sortIdx(candidates_inliers, candidates_inliers_idx, CV_SORT_DESCENDING);
+		sortAndKeepIdx(candidates_inliers, candidates_inliers_idx,
+				CV_SORT_DESCENDING);
 
 		// Copying re-ranked candidates
 		geom_ranked_candidates_list.clear();
 		for (size_t j = 0; int(j) < top; ++j) {
 			geom_ranked_candidates_list.push_back(
-					ranked_candidates_list[candidates_inliers_idx.at<int>(0, j)]);
+					ranked_candidates_list[candidates_inliers_idx[j]]);
 		}
 
 #if GVVERBOSE
@@ -418,6 +368,28 @@ int main(int argc, char **argv) {
 				geom_ranked_candidates_list.size());
 	}
 
-	HtmlResultsWriter::getInstance().close();
+//	HtmlResultsWriter::getInstance().close();
+
+}
+
+template<typename T>
+void sortAndKeepIdx(std::vector<T>& values, std::vector<size_t>& indices,
+		int flags) {
+
+	indices.clear();
+	indices.resize(values.size());
+
+	for (size_t i = 0; i < indices.size(); ++i) {
+		indices[i] = i;
+	}
+
+	if (flags == CV_SORT_ASCENDING) {
+		std::stable_sort(indices.begin(), indices.end(),
+				[&](size_t a, size_t b) {return values[a] < values[b];});
+	} else {
+		// Sort descending by default
+		std::stable_sort(indices.begin(), indices.end(),
+				[&](size_t a, size_t b) {return values[a] > values[b];});
+	}
 
 }
