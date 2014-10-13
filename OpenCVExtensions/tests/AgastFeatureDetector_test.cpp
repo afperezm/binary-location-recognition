130c22e8f704cd32c7c08f7bfcfd6c5434ac4c7f
Andres Perez <gantzer89@gmail.com>
2014-10-13 10:52:30 +0200

    Setup test for implementations of agast and dbrief OpenCV wrappers.

--- OpenCVExtensions/tests/AgastFeatureDetector_test.cpp
+++ OpenCVExtensions/tests/AgastFeatureDetector_test.cpp
@@ -7,3 +7,45 @@
 
 #include <AgastFeatureDetector.h>
 
+#include <gtest/gtest.h>
+#include <opencv2/extensions/features2d.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+TEST(AgastFeatureDetector, Init) {
+
+	cv::initModule_features2d_extensions();
+
+	std::vector<std::string> algorithms;
+	cv::Algorithm::getList(algorithms);
+
+	std::string candidateAlgorithm = "Feature2D.AGAST";
+
+	bool isValid = false;
+
+	for (std::string& algorithm : algorithms) {
+		isValid |= algorithm.compare(candidateAlgorithm) == 0;
+	}
+
+	ASSERT_TRUE(isValid);
+
+}
+
+TEST(AgastFeatureDetector, Detect) {
+
+	cv::Mat img = cv::imread("test.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+
+	cv::Ptr<cv::FeatureDetector> detector = cv::FeatureDetector::create(
+			"AGAST");
+
+	ASSERT_TRUE(detector->name().compare("Feature2D.AGAST") == 0);
+
+	std::vector<cv::KeyPoint> keypoints;
+
+	ASSERT_TRUE(keypoints.size() == 0);
+
+	detector->detect(img, keypoints);
+
+	ASSERT_TRUE(keypoints.size() != 0);
+
+}
--- OpenCVExtensions/tests/DBriefDescriptorExtractor_test.cpp
+++ OpenCVExtensions/tests/DBriefDescriptorExtractor_test.cpp
@@ -5,8 +5,50 @@
  *      Author: andresf
  */
 
-#include <include/DBriefDescriptorExtractor.h>
+#include <DBriefDescriptorExtractor.h>
 
-namespace cv {
+#include <gtest/gtest.h>
+#include <opencv2/extensions/features2d.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-} /* namespace cv */
+TEST(DBriefFeatureExtractor,Init) {
+
+	cv::initModule_features2d_extensions();
+
+	std::vector<std::string> algorithms;
+	cv::Algorithm::getList(algorithms);
+
+	std::string candidateAlgorithm = "Feature2D.DBRIEF";
+
+	bool isValid = false;
+
+	for (std::string& algorithm : algorithms) {
+		isValid |= algorithm.compare(candidateAlgorithm) == 0;
+	}
+
+	ASSERT_TRUE(isValid);
+
+}
+
+TEST(DBriefFeatureExtractor,Extract) {
+
+	cv::Mat img = cv::imread("test.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+
+	cv::Ptr<cv::FeatureDetector> detector = cv::FeatureDetector::create("FAST");
+
+	std::vector<cv::KeyPoint> keypoints;
+	detector->detect(img, keypoints);
+
+	cv::Ptr<cv::DescriptorExtractor> extractor =
+			cv::DescriptorExtractor::create("DBRIEF");
+
+	cv::Mat descriptors;
+
+	ASSERT_TRUE(descriptors.empty());
+
+	extractor->compute(img, keypoints, descriptors);
+
+	ASSERT_FALSE(descriptors.empty());
+
+}
