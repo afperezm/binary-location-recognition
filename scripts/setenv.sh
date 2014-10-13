07d28b06cc23aaeb9e9fd799befa0000803a8b54
Andres Perez <gantzer89@gmail.com>
2014-10-13 11:23:35 +0200

    Exported ListBuild to the system path

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -2,7 +2,7 @@
 
 export APP_ROOT=$HOME/workspace-cpp/VLRPipeline
 
-export PATH=$APP_ROOT/VocabLearn/:$APP_ROOT/VocabBuildDB/:$APP_ROOT/VocabMatch/:$APP_ROOT/FeatureExtract/:$PATH:$APP_ROOT/ComputeMAP:$APP_ROOT/SelectDescriptors:$APP_ROOT/FeatureSelect:$APP_ROOT/GeomVerify:$APP_ROOT/OxDataReader
+export PATH=$APP_ROOT/VocabLearn/:$APP_ROOT/VocabBuildDB/:$APP_ROOT/VocabMatch/:$APP_ROOT/FeatureExtract/:$PATH:$APP_ROOT/ComputeMAP:$APP_ROOT/SelectDescriptors:$APP_ROOT/FeatureSelect:$APP_ROOT/GeomVerify:$APP_ROOT/OxDataReader:$APP_ROOT/ListBuild
 
 export LD_LIBRARY_PATH=$APP_ROOT/lib:$LD_LIBRARY_PATH
 
