dcaf55ff3c5d0ce9cb99815190271e382bd5cb73
Andres Perez <a.perez@digitelematica.com>
2015-06-02 00:05:52 -0500

    Implement libincrementalkmeans tests

--- IncrementalKMeansLib/tests/IncrementalKMeans_test.cpp
+++ IncrementalKMeansLib/tests/IncrementalKMeans_test.cpp
@@ -8,30 +8,119 @@
 #include <gtest/gtest.h>
 
 #include <FileUtils.hpp>
-#include <IncrementalKMeans.h>
+#include <FunctionUtils.hpp>
+#include <IncrementalKMeans.hpp>
 
 namespace vlr {
 
-TEST(IncrementalKMeans, Instantiation) {
+TEST(IncrementalKMeans, InitWithFakeData) {
 
-	cv::Mat emptyMat;
-	vlr::IncrementalKMeans obj(emptyMat);
+	cv::Mat data(3, 1, CV_8U);
+
+	data.at<uchar>(0, 0) = 4;
+	data.at<uchar>(0, 1) = 10;
+	data.at<uchar>(0, 2) = 5;
+
+	vlr::IncrementalKMeans obj(data);
+
+	EXPECT_TRUE(obj.getDim() == data.cols);
+
+	EXPECT_TRUE(obj.getNumDatapoints() == data.rows);
+
+	EXPECT_TRUE(obj.getDataset().rows == data.rows);
+	EXPECT_TRUE(obj.getDataset().cols == data.cols);
+
+	EXPECT_TRUE(obj.getCentroids().rows == obj.getNumClusters());
+	EXPECT_TRUE(obj.getCentroids().cols == data.cols * 8);
+
+	EXPECT_TRUE(obj.getClustersVariances().rows == obj.getNumClusters());
+	EXPECT_TRUE(obj.getClustersVariances().cols == data.cols * 8);
+
+	EXPECT_TRUE(obj.getClustersWeights().rows == 1);
+	EXPECT_TRUE(obj.getClustersWeights().cols == obj.getNumClusters());
+
+	EXPECT_TRUE(obj.getClustersSums().rows == obj.getNumClusters());
+	EXPECT_TRUE(obj.getClustersSums().cols == data.cols * 8);
+
+	EXPECT_TRUE(obj.getClustersCounts().rows == 1);
+	EXPECT_TRUE(obj.getClustersCounts().cols == obj.getNumClusters());
+
+	EXPECT_TRUE(obj.getMiu().rows == 1);
+	EXPECT_TRUE(obj.getMiu().cols == data.cols * 8);
+
+	EXPECT_TRUE(obj.getSigma().rows == 1);
+	EXPECT_TRUE(obj.getSigma().cols == data.cols * 8);
+
+	EXPECT_TRUE(obj.getMiu().at<double>(0, 0) == ((double) 0.0));
+	EXPECT_TRUE(obj.getMiu().at<double>(0, 1) == ((double) 0.0));
+	EXPECT_TRUE(obj.getMiu().at<double>(0, 2) == ((double) 0.0));
+	EXPECT_TRUE(obj.getMiu().at<double>(0, 3) == ((double) 0.0));
+	EXPECT_TRUE(obj.getMiu().at<double>(0, 4) == ((double) 1/3));
+	EXPECT_TRUE(obj.getMiu().at<double>(0, 5) == ((double) 2/3));
+	EXPECT_TRUE(obj.getMiu().at<double>(0, 6) == ((double) 1/3));
+	EXPECT_TRUE(obj.getMiu().at<double>(0, 7) == ((double) 1/3));
+
+	EXPECT_TRUE(obj.getSigma().at<double>(0, 0) == ((double) 0.0));
+	EXPECT_TRUE(obj.getSigma().at<double>(0, 1) == ((double) 0.0));
+	EXPECT_TRUE(obj.getSigma().at<double>(0, 2) == ((double) 0.0));
+	EXPECT_TRUE(obj.getSigma().at<double>(0, 3) == ((double) 0.0));
+	EXPECT_TRUE(obj.getSigma().at<double>(0, 4) == sqrt((double) 6/27));
+	EXPECT_TRUE(obj.getSigma().at<double>(0, 5) == sqrt((double) 6/27));
+	EXPECT_TRUE(obj.getSigma().at<double>(0, 6) == sqrt((double) 6/27));
+	EXPECT_TRUE(obj.getSigma().at<double>(0, 7) == sqrt((double) 6/27));
+
+	EXPECT_TRUE(obj.getOutliers().empty());
 
 }
 
-TEST(IncrementalKMeans, Constructor) {
+TEST(IncrementalKMeans, InitWithRealData) {
 
-	cv::Mat imgDescriptors;
+	cv::Mat data;
+	FileUtils::loadDescriptors("brief_0.bin", data);
 
-	FileUtils::loadDescriptors("sift_0.bin", imgDescriptors);
+	vlr::IncrementalKMeans obj(data);
 
-	/*
-	 std::vector<std::string> keysFilenames(1, "sift_0.bin");
-	 vlr::Mat data(keysFilenames);
-	 */
+	EXPECT_TRUE(obj.getDim() == data.cols);
 
-	vlr::IncrementalKMeans obj(imgDescriptors);
+	EXPECT_TRUE(obj.getNumDatapoints() == data.rows);
+
+	EXPECT_TRUE(obj.getDataset().rows == data.rows);
+	EXPECT_TRUE(obj.getDataset().cols == data.cols);
+
+	EXPECT_TRUE(obj.getCentroids().rows == obj.getNumClusters());
+	EXPECT_TRUE(obj.getCentroids().cols == data.cols * 8);
+
+	EXPECT_TRUE(obj.getClustersVariances().rows == obj.getNumClusters());
+	EXPECT_TRUE(obj.getClustersVariances().cols == data.cols * 8);
+
+	EXPECT_TRUE(obj.getClustersWeights().rows == 1);
+	EXPECT_TRUE(obj.getClustersWeights().cols == obj.getNumClusters());
+
+	EXPECT_TRUE(obj.getClustersSums().rows == obj.getNumClusters());
+	EXPECT_TRUE(obj.getClustersSums().cols == data.cols * 8);
+
+	EXPECT_TRUE(obj.getClustersCounts().rows == 1);
+	EXPECT_TRUE(obj.getClustersCounts().cols == obj.getNumClusters());
+
+	EXPECT_TRUE(obj.getMiu().rows == 1);
+	EXPECT_TRUE(obj.getMiu().cols == data.cols * 8);
+
+	EXPECT_TRUE(obj.getSigma().rows == 1);
+	EXPECT_TRUE(obj.getSigma().cols == data.cols * 8);
+
+	EXPECT_TRUE(obj.getOutliers().empty());
 
 }
+
+//TEST(IncrementalKMeans, Clustering) {
+//
+//	cv::Mat descriptors;
+//	FileUtils::loadDescriptors("brief_0.bin", descriptors);
+//
+//	vlr::IncrementalKMeans obj(descriptors);
+//
+//	obj.build();
+//
+//}
 
 } /* namespace vlr */
